e84987b46c73d48977edd34754902a95b3d68e86
Lakshmisha <lakshmisha.th@gmail.com>
2019-08-20 06:54:12 +0000

    using kitchen.yaml

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -1,16 +1,6 @@
-# InSpec test for recipe workstation::default
+system_policies = 'HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System'
 
-# The InSpec reference, with examples and extensive documentation, can be
-# found at https://www.inspec.io/docs/reference/resources/
-
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
-end
-
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe registry_key('System Policies', system_policies) do
+  its('EnableLUA') { should eq 0 }
+  its('ConsentPromptBehaviorAdmin') { should eq 0 }
 end
